bbaf41271a75d8530c3ec2af3ea45b3ddead7528
Lucas Goeten <lucas.goeten@partners.mv.com.br>
2021-12-10 10:32:33 -0300

    Referencias categorias produtos

--- src/main/java/com/estudo/springstudy/SpringStudyApplication.java
+++ src/main/java/com/estudo/springstudy/SpringStudyApplication.java
@@ -1,13 +1,51 @@
 package com.estudo.springstudy;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringStudyApplication {
+import com.estudo.springstudy.domain.Categoria;
+import com.estudo.springstudy.domain.Produto;
+import com.estudo.springstudy.repository.CategoriaRepository;
+import com.estudo.springstudy.repository.ProdutoRepository;
 
+@SpringBootApplication
+public class SpringStudyApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringStudyApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
 	}
 
 }
