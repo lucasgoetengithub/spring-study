02adcbccf22d5e1615d3bab39fc843e45726c3ec
Lucas Goeten <lucas.goeten@partners.mv.com.br>
2021-12-10 11:25:43 -0300

    object not found exception

--- src/main/java/com/estudo/springstudy/service/CategoriaService.java
+++ src/main/java/com/estudo/springstudy/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.estudo.springstudy.domain.Categoria;
 import com.estudo.springstudy.repository.CategoriaRepository;
+import com.estudo.springstudy.service.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,7 @@ public class CategoriaService {
 	
 	public Categoria Find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id + ", tipo " + Categoria.class.getName(), null));
 	}
 	
 }
