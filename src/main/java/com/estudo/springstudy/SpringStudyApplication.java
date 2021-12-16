f570a8a9fec25edd238cfbd333cadde827388e56
Lucas Goeten <lucasgoetengithub@gmail.com>
2021-12-16 14:19:00 -0300

    paginação categoria

--- src/main/java/com/estudo/springstudy/SpringStudyApplication.java
+++ src/main/java/com/estudo/springstudy/SpringStudyApplication.java
@@ -35,31 +35,31 @@ public class SpringStudyApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+
 	@Autowired
 	private CidadeRepository cidadeRepository;
-	
+
 	@Autowired
 	private EstadoRepository estadoRepository;
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	@Autowired
 	private EnderecoRepository enderecoRepository;
-	
+
 	@Autowired
 	private PedidoRepository pedidoRepository;
-	
+
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
-	
+
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringStudyApplication.class, args);
 	}
@@ -68,61 +68,74 @@ public class SpringStudyApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+		Categoria cat3 = new Categoria(null, "Cama mesa e banho");
+		Categoria cat4 = new Categoria(null, "Quarto");
+		Categoria cat5 = new Categoria(null, "Sala de estar");
+		Categoria cat6 = new Categoria(null, "Sala de jantar");
+		Categoria cat7 = new Categoria(null, "Cozinha");
+		Categoria cat8 = new Categoria(null, "Lavação");
+		Categoria cat9 = new Categoria(null, "Quarto de criança");
+		Categoria cat10 = new Categoria(null, "Brinquedos");
+		Categoria cat11 = new Categoria(null, "Tapeçaria");
+		Categoria cat12 = new Categoria(null, "Eletrodomesticos");
+		Categoria cat13 = new Categoria(null, "Decoração");
+
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
-		
+
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
-		
+
 		p1.getCategorias().addAll(Arrays.asList(cat1));
-		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+		categoriaRepository.saveAll(
+				Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8, cat9, cat10, cat11, cat12, cat13));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
-		
+
 		Estado est1 = new Estado(null, "Santa Catarina");
 		Estado est2 = new Estado(null, "Sao Paulo");
-		
+
 		Cidade c1 = new Cidade(null, "Indaial", est1);
 		Cidade c2 = new Cidade(null, "Sao Paulo", est2);
 		Cidade c3 = new Cidade(null, "Blumenau", est1);
-		
+
 		est1.getCidades().addAll(Arrays.asList(c1, c3));
 		est2.getCidades().addAll(Arrays.asList(c2));
-		
+
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
-		
-		Cliente cli1 = new Cliente(null, "Lucas Eduardo Goeten", "lucasgoeten@email.com", "32165498764",TipoCliente.PESSOA_FISICA);
+
+		Cliente cli1 = new Cliente(null, "Lucas Eduardo Goeten", "lucasgoeten@email.com", "32165498764",
+				TipoCliente.PESSOA_FISICA);
 		cli1.getTelefones().addAll(Arrays.asList("321654", "1234522"));
-		
+
 		Endereco end1 = new Endereco(null, "Rua avenida almeira", "300", "apto 1", "Jardim", "64654", cli1, c1);
 		cli1.getEnderecos().addAll(Arrays.asList(end1));
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2021 10:32"), cli1, end1);
-		
+
 		Pagamento pag1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
 		ped1.setPagamento(pag1);
-		
+
 		cli1.getPedidos().addAll(Arrays.asList(ped1));
-		
+
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(end1));
-		
+
 		pedidoRepository.saveAll(Arrays.asList(ped1));
 		pagamentoRepository.saveAll(Arrays.asList(pag1));
-		
+
 		ItemPedido itemPedido1 = new ItemPedido(ped1, p3, 10.00, 1, 3000.00);
 		ItemPedido itemPedido2 = new ItemPedido(ped1, p2, 10.00, 1, 3000.00);
-		
+
 		ped1.getItemPedidos().addAll(Arrays.asList(itemPedido1, itemPedido2));
 		p1.getItemPedidos().addAll(Arrays.asList(itemPedido1));
 		p2.getItemPedidos().addAll(Arrays.asList(itemPedido2));
-		
+
 		itemPedidoRepository.saveAll(Arrays.asList(itemPedido1, itemPedido2));
 	}
 
--- src/main/java/com/estudo/springstudy/service/CategoriaService.java
+++ src/main/java/com/estudo/springstudy/service/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.estudo.springstudy.domain.Categoria;
@@ -17,22 +20,23 @@ public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id + ", tipo " + Categoria.class.getName(), null));
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! ID: " + id + ", tipo " + Categoria.class.getName(), null));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
 		try {
@@ -41,8 +45,13 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
 	}
-	
+
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
