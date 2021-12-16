a594687f44d416cdb56b2b8728eee7610357f626
Lucas Goeten <lucasgoetengithub@gmail.com>
2021-12-16 12:43:55 -0300

    Categoria DTO

--- src/main/java/com/estudo/springstudy/resources/CategoriaResource.java
+++ src/main/java/com/estudo/springstudy/resources/CategoriaResource.java
@@ -2,6 +2,8 @@ package com.estudo.springstudy.resources;
 
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.estudo.springstudy.domain.Categoria;
+import com.estudo.springstudy.dto.CategoriaDTO;
 import com.estudo.springstudy.service.CategoriaService;
 
 @RestController
@@ -46,5 +49,12 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		categoriaService.delete(id);		
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> categorias = categoriaService.findAll();		
+		List<CategoriaDTO> listDto = categorias.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
