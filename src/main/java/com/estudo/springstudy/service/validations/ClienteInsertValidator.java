d1a4157f2e5ea352fb5b90b7c3600d8fcab38546
Lucas Goeten <lucasgoetengithub@gmail.com>
2021-12-20 16:05:40 -0300

    validação email repetido

--- src/main/java/com/estudo/springstudy/service/validations/ClienteInsertValidator.java
+++ src/main/java/com/estudo/springstudy/service/validations/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.estudo.springstudy.domain.Cliente;
 import com.estudo.springstudy.domain.enums.TipoCliente;
 import com.estudo.springstudy.dto.ClienteNewDTO;
+import com.estudo.springstudy.repository.ClienteRepository;
 import com.estudo.springstudy.resources.exceptions.FieldMessage;
 import com.estudo.springstudy.service.validations.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {	
@@ -26,6 +33,13 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		} else if (objDto.getTipoCliente().equals(TipoCliente.PESSOA_JURIDICA.getCode()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("CpfOuCnpj", "CNPJ inválido"));
 		}
+		
+		
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		if (aux != null ) {
+			list.add(new FieldMessage("Email", "Email já cadastrado"));
+		}
+		
 		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
