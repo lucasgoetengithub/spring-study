c3c33376d96f142aa061092e8edcad4e396db7d3
Lucas Goeten <lucasgoetengithub@gmail.com>
2021-12-21 11:26:40 -0300

    JPQL produtos

--- src/main/java/com/estudo/springstudy/service/ProdutoService.java
+++ src/main/java/com/estudo/springstudy/service/ProdutoService.java
@@ -1,11 +1,17 @@
 package com.estudo.springstudy.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.estudo.springstudy.domain.Categoria;
 import com.estudo.springstudy.domain.Produto;
+import com.estudo.springstudy.repository.CategoriaRepository;
 import com.estudo.springstudy.repository.ProdutoRepository;
 import com.estudo.springstudy.service.exception.ObjectNotFoundException;
 
@@ -14,9 +20,18 @@ public class ProdutoService {
 	@Autowired
 	private ProdutoRepository repo;
 
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public Produto find(Integer id) {
 		Optional<Produto> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! ID: " + id + ", tipo " + Produto.class.getName(), null));
+	}
+	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repo.search(nome, categorias, pageRequest);
 	}
 }
