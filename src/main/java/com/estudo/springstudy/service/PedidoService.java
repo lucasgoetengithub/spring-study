802c5f9a8c1bf57269a1c9944f190bb40614e507
Lucas Goeten <lucasgoetengithub@gmail.com>
2021-12-27 09:27:45 -0300

    JWT restrição de conteudo para pedidos e clientes

--- src/main/java/com/estudo/springstudy/service/PedidoService.java
+++ src/main/java/com/estudo/springstudy/service/PedidoService.java
@@ -6,8 +6,12 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.estudo.springstudy.domain.Cliente;
 import com.estudo.springstudy.domain.ItemPedido;
 import com.estudo.springstudy.domain.PagamentoComBoleto;
 import com.estudo.springstudy.domain.Pedido;
@@ -16,6 +20,8 @@ import com.estudo.springstudy.repository.ItemPedidoRepository;
 import com.estudo.springstudy.repository.PagamentoRepository;
 import com.estudo.springstudy.repository.PedidoRepository;
 import com.estudo.springstudy.repository.ProdutoRepository;
+import com.estudo.springstudy.security.UserSS;
+import com.estudo.springstudy.service.exception.AuthorizationException;
 import com.estudo.springstudy.service.exception.ObjectNotFoundException;
 
 @Service
@@ -23,24 +29,28 @@ public class PedidoService {
 
 	@Autowired
 	private PedidoRepository repo;
-	
+
 	@Autowired
 	private BoletoService boletoService;
-	
+
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
-	
+
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
 	
+	@Autowired
+	private ClienteService clienteService;
+
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id + ", tipo " + Pedido.class.getName(), null));
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! ID: " + id + ", tipo " + Pedido.class.getName(), null));
 	}
-	
+
 	@Transactional
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
@@ -51,18 +61,28 @@ public class PedidoService {
 			PagamentoComBoleto pagamentoComBoleto = (PagamentoComBoleto) obj.getPagamento();
 			boletoService.preencherPagamentoComBoleto(pagamentoComBoleto, obj.getInstante());
 		}
-		
+
 		obj = repo.save(obj);
 		pagamentoRepository.save(obj.getPagamento());
-		
+
 		for (ItemPedido ip : obj.getItemPedidos()) {
 			ip.setDesconto(0.0);
 			ip.setPreco(produtoRepository.findById(ip.getProduto().getId()).get().getPreco());
 			ip.setPedido(obj);
 		}
-		
+
 		itemPedidoRepository.saveAll(obj.getItemPedidos());
-		
+
 		return repo.save(obj);
+	}
+
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS ss = UserService.authenticated();
+		if (ss == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(ss.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
